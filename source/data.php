d82f44dea3f2f83b4084c01e1cfee7ce4369a1df
Tarmiko <tarmikodeveloper@gmail.com>
2023-03-21 14:21:16 +0700

    Appearance updates and few samples added

---
+++ source/data.php
@@ -0,0 +1,16 @@
+<?php
+	$uri = $_SERVER["REQUEST_URI"];
+	include "." . substr($uri, strlen("/urbania/source"));
+
+	$types = array(
+		"css" => "text/css",
+		"html" => "text/html",
+		"php" => "text/html"
+	);
+	$type = array_pop(explode('.', $uri));
+
+	if($type = $types[$type]){
+		header("Content-Type: " . $type);
+	}
+	/*echo substr($_SERVER["REQUEST_URI"], strlen("/urbania/source"));*/
+?>
